0a88fe5de0b33a25188b39c6099ba93e213933d8
Alexsandro Morais <alexsandro.almeida1987@gmail.com>
2026-06-26 11:05:50 -0300

    chore: auto-save local 1-min (daemon global) [skip ci]

--- app/scratch.cjs
+++ app/scratch.cjs
@@ -1,5 +1,10 @@
 const XLSX = require('xlsx');
-const wb = XLSX.readFile('C:/Users/Alexsandro Morais/ABR GERENCIAMENTO E ENGENHARIA LTDA/LYO004 SIMOES FILHO G200 - Documentos/03. CUSTOS/3.0 MEDIÇÕES APROVADAS E NF\'S/1. ROMANEIOS/0.APROVADOS/PEDIDOS/sienge_relatorio-PEDIDOS.xlsx');
+const wb = XLSX.readFile('C:/Users/Alexsandro Morais/ABR GERENCIAMENTO E ENGENHARIA LTDA/LYO004 SIMOES FILHO G200 - Documentos/03. CUSTOS/3.0 MEDIÇÕES APROVADAS E NF\'S/1. ROMANEIOS/0.APROVADOS/PEDIDOS/BASE-PEDIDOS.xlsx');
 const ws = wb.Sheets[wb.SheetNames[0]];
 const data = XLSX.utils.sheet_to_json(ws, {header: 1});
-console.log(data[0]);
+console.log('BASE-PEDIDOS:', data[0]);
+
+const wbZ = XLSX.readFile('C:/Users/Alexsandro Morais/ABR GERENCIAMENTO E ENGENHARIA LTDA/LYO004 SIMOES FILHO G200 - Documentos/03. CUSTOS/3.0 MEDIÇÕES APROVADAS E NF\'S/1. ROMANEIOS/0.APROVADOS/PEDIDOS/ZEPP_Analítico_Processos_Aprovações.xlsx');
+const wsZ = wbZ.Sheets[wbZ.SheetNames[0]];
+const dataZ = XLSX.utils.sheet_to_json(wsZ, {header: 1});
+console.log('ZEPP:', dataZ[0]);
