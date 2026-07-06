e7cf0738467743e02fa9d7a7f6effd82d69a03f3
Alexsandro Morais <alexsandro.almeida1987@gmail.com>
2026-07-06 10:34:27 -0300

    chore: auto-save local 1-min (daemon global) [skip ci]

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/ferramenta-conciliacao-pmg/', // Base URL para o GitHub Pages
+  base: '/', // Base URL para a Vercel
   plugins: [
     react(),
     VitePWA({
