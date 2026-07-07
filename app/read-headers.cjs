651be355636ffd24d8f631d79d42f3a74528bb40
Alexsandro Morais <alexsandro.almeida1987@gmail.com>
2026-07-07 14:51:08 -0300

    chore: auto-save local 1-min (daemon global) [skip ci]

---
+++ app/read-headers.cjs
@@ -0,0 +1,5 @@
+const XLSX = require('xlsx');
+const workbook = XLSX.readFile("C:/Users/Alexsandro Morais/ABR GERENCIAMENTO E ENGENHARIA LTDA/LYO004 SIMOES FILHO G200 - Documentos/03. CUSTOS/3.0 MEDIÇÕES APROVADAS E NF'S/1. ROMANEIOS/0.APROVADOS/TÍTULOS/TÍTULOS SIENGE.xlsx");
+const sheet = workbook.Sheets[workbook.SheetNames[0]];
+const json = XLSX.utils.sheet_to_json(sheet, {header: 1});
+console.log(json[0]);
